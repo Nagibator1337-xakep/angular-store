89df61f18f5c5607c341c55786c010eb038081a3
Nagibator1337-xakep <MamkuIbalAzaza1337>
2022-02-07 21:02:19 +0500

    Fix merge issues

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder } from "@angular/forms";
 import { CartService } from "../cart.service";
 import { Location } from "@angular/common";
-<<<<<<< HEAD
 import { Product } from "../products";
-=======
->>>>>>> 4622816777a50e628c5f8e2b70db85da1b5e6683
 
 @Component({
   selector: "app-cart",
@@ -25,27 +22,18 @@ export class CartComponent implements OnInit {
     private location: Location,
     private formBuilder: FormBuilder
   ) {}
-<<<<<<< HEAD
 
   ngOnInit(): void {}
 
-=======
-
-  ngOnInit(): void {}
-
->>>>>>> 4622816777a50e628c5f8e2b70db85da1b5e6683
   onSubmit(): void {
     // Process checkout data here
     this.items = this.cartService.clearCart();
     console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
-<<<<<<< HEAD
   }
 
   removeFromCart(product: Product) {
     this.cartService.removeFromCart(product);
-=======
->>>>>>> 4622816777a50e628c5f8e2b70db85da1b5e6683
   }
 
   back(): void {
